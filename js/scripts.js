a782ef5f173618a42e6394c31961524e817bb05a
Rhael Henrique <rhaelhenriquerodrigues@gmail.com>
2021-05-20 17:28:50 -0300

    Melhorias

--- js/scripts.js
+++ js/scripts.js
@@ -2,4 +2,22 @@ function contar(){
     var chars = document.getElementById("texto").value;
 
     document.getElementById("caracteres").innerHTML = chars.length;
+
+    var fullString = chars + " ";
+    var primeiroEspacoVazio = /^[^\wÀ-ú]+/gi;
+    var retirarEspacoVazio = fullString.replace(primeiroEspacoVazio, "");
+    var charsNaoAlfaNumericos = charsOther = /[^\wÀ-ú']+/gi;
+    var stringLimpa = retirarEspacoVazio.replace(charsNaoAlfaNumericos, " ");
+    var contaEspaco = stringLimpa.split(" ");
+    var words = contaEspaco.length - 1;
+    
+    var line = chars.split("\n");
+    var lines = (chars.length==0) ? 0 : line.length;
+
+    document.getElementById("palavras").innerHTML = words;
+    document.getElementById("linhas").innerHTML = lines;
+}
+
+function uper(){
+    alert("teste");
 }
